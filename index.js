7cc3e98ed37aedca595e617c3382d84d209f0c17
harshit-noovosoft <harshit.manavadariya@noovosoft.com>
2023-09-10 19:07:57 +0530

    Create Login and Register Pages

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require('express');
-require('dotenv').config();
+import express from 'express';
+import dotenv from "dotenv"
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import jwt from "jsonwebtoken";
+const __dirname = path.resolve();
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+import authentication from "./routes/authentication.js";
+import transaction from "./routes/transaction.js";
+
+app.use("/authentication" , authentication);
+app.use("/transaction" , transaction);
+
+app.use((req,res,next)=>{
+    res.clearCookie('access_token');
+    const token = req.cookies['access_token'];
+    if(!token) {
+        return res.redirect('/authentication/login');
+    }
+    next();
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname , './public/home.html'));
+});
 
 app.listen(process.env.PORT , (req,res)=>{
     console.log(`Server running on PORT ${process.env.PORT}`);

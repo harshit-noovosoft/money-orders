e19f16392e0c4883d204e0646893ee3cc353510b
harshit-noovosoft <harshit.manavadariya@noovosoft.com>
2023-09-11 11:15:01 +0530

    Create all transaction api

---
+++ routes/transaction.js
@@ -0,0 +1,56 @@
+import express from "express";
+import pool from "../database_connection.js";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.use((req,res,next)=>{
+    const token = req.cookies['access_token'];
+    if(!token) {
+        return res.status(400).send("Token not found");
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
+        if (err || payload.role !== 'admin') {
+            return res.status(401).json(err.message || "You are not admin");
+        }
+        req.user = {
+            username: payload.username,
+            role: payload.role
+        }
+        next();
+    });
+});
+
+router.get('/' , async (req,res)=>{
+    try{
+        const transactions = await pool.query(`SELECT transaction_id,
+                                                      transaction_type,
+                                                      (SELECT username from users 
+                                                                       where user_id = transactions.from_user_id) 
+                                                          as from_user,
+                                                      (SELECT username from users 
+                                                                       where user_id = transactions.to_user_id)   
+                                                          as to_user
+                                               from transactions`);
+        res.send("Harshit");
+    }catch (err){
+        res.status(err.status || 400).send(err.message);
+    }
+});
+
+router.post('/' , async (req,res)=>{
+    const { from_user_id , amount , type , to_user_id} = req.body;
+    try{
+        await pool.query(`Insert into transactions (
+                   transaction_type, from_user_id, to_user_id,amount) 
+                        values ($1,$2,$3,$4)`,
+            [type,from_user_id,to_user_id,amount]
+        );
+        res.send("Transaction Successfull");
+    }catch (err){
+        res.status(err.status || 500).send(err.message);
+    }
+});
+
+export default router;

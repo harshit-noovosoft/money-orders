6887e6d14ca3683cefa05d9d9c0d5473a374ce2c
shreyash-noovosoft <shreyash.deshpande@noovosoft.com>
2023-09-14 12:05:45 +0530

    Refactor merge conflicts of mail

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -21,42 +21,72 @@ router.get('/'  , verifyToken, async (req,res) => {
         [username]
     );
 
+
+    function getTableData(data) {
+        const headers = ['Type','From','To','Amount'];
+        const tableHeaders = headers.map(header => `<th>${header}</th>`).join('');
+        const tableRows = data.map(row =>
+            `<tr>${headers.map(header => `<td>${row[header] || ""}</td>`).join('')}</tr>`
+        ).join('');
+
+
+        return `
+        <table border="1">
+          <thead>
+            <tr>${tableHeaders}</tr>
+          </thead>
+          <tbody>
+            ${tableRows}
+          </tbody>
+        </table>
+    `;
+    }
+
+    function generateTabularFormOfData(transaction) {
+        const tableHTML = getTableData(transaction);
+        return `
+            <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <title></title>
+                    <style>
+                        td{
+                            padding: 5px 10px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>Transaction List</h1>
+                    ${tableHTML}
+                </body>
+            </html>
+        `
+    }
+
     const userId = result.rows[0].user_id;
     const to_email = result.rows[0].email;
     const rows_limit = req.query.limit
 
-    const queryString = `SELECT transactions.transaction_id,
-                                                      transactions.transaction_type,
-                                                      (SELECT username from users
-                                                                       where user_id = transactions.from_user_id)
-                                                          as from_user,
-                                                      (SELECT username from users
-                                                                       where user_id = transactions.to_user_id)
-                                                          as to_user, transactions.amount
-                                               from transactions
-                                                WHERE from_user_id = $1 or to_user_id = $1
-                                                ORDER BY transaction_id DESC
-                                                LIMIT $2`
+    const queryString = `
+                    SELECT transactions.transaction_type as "Type",
+                                (SELECT username from users
+                                 where user_id = transactions.from_user_id)
+                                                              as "From" ,
+                                (SELECT username from users
+                                 where user_id = transactions.to_user_id)
+                                                              as "To", transactions.amount as "Amount"
+                         from transactions
+                         WHERE from_user_id = $1 or to_user_id = $1
+                         ORDER BY transaction_id DESC
+                         LIMIT $2`
     const transactions = await pool.query(queryString , [userId, rows_limit]);
+    const allTransactions = generateTabularFormOfData(transactions.rows);
 
-    let table = "<table border='1px'>";
-    table += "<thead><td>ID</td><td>Type</td><td>From</td><td>To</td><td>amount</td></thead>"
-    table += "<tbody>";
-    transactions.rows.forEach((transaction) => {
-        table += "<tr>"
-        table += "<td>" + transaction.transaction_id +"</td>"
-        table += "<td>" + transaction.transaction_type +"</td>"
-        table += "<td>" + transaction.from_user +"</td>"
-        table += "<td>" + transaction.to_user +"</td>"
-        table += "<td>" + transaction.amount +"</td>"
-        table += "</tr>"
-    })
-    table += "</tbody></table>"
     const obj = await transporter.sendMail({
         from: "admin@gmail.com",
         to: to_email,
         subject: "Requested Transactions List",
-        html: table
+        html: allTransactions
     }).then();
     if(obj) {
         return res.send({status: 200});

addfffc44502f8685da0c3cce32db7b52bf9b292
harshit-noovosoft <harshit.manavadariya@noovosoft.com>
2023-09-15 13:37:59 +0530

    Refactor mail feature from front-end

--- public/js/sendMail.js
+++ public/js/sendMail.js
@@ -1,19 +1,57 @@
 const mailBtn = document.getElementById('get-mail')
 
+async function loadEmails() {
+    const response = await fetch(BASE_URL + "sendMail");
+    return await response.json();
+}
+
 mailBtn.addEventListener('click', async function (e) {
     e.preventDefault()
-    let limit = document.getElementById('transaction_limit')
-    await fetch(BASE_URL + 'sendMail/?limit=' + limit.value, {
-        method: "GET",
+    let limit = document.getElementById('transaction_limit').value;
+    await fetch(BASE_URL + 'sendMail', {
+        method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
+        body: JSON.stringify({limit: limit})
     }).then((res) => {
         if(res.status === 200) {
-            alert('Mail sent successfully')
+            alert('Mail sent successfully');
+            loadNewEmails();
         }else {
             alert('Some error occurred at backend')
         }
     })
-    limit.value = ""
-})
+    limit.value = "";
+});
+
+function createEmailTable(rows) {
+    const emails =  rows;
+    const tbl = document.getElementById('email-table');
+    const previousRows = tbl.querySelector("tbody")
+    console.log(previousRows);
+    if(previousRows) {
+        previousRows.remove()
+    }
+    const tblBody = document.createElement("tbody");
+    emails.reverse().forEach((email) => {
+        const rowData = [
+            email.sender_email,
+            email.receiver_email,
+            email.transaction_limit,
+            email.email_status
+        ]
+        tblBody.appendChild(createRow(rowData));
+    })
+    tbl.appendChild(tblBody);
+}
+
+function loadNewEmails() {
+    loadEmails().then(res => {
+        if(res.role !== 'customer') return;
+        createEmailTable(res.rows);
+    })
+}
+
+loadNewEmails();
+
--- routes/transaction.js
+++ routes/transaction.js
@@ -17,34 +17,23 @@ const operationalMiddleware = ((req,res,next)=>{
 router.get('/'  ,async (req,res)=>{
     try{
         const {username , role} = req.user;
-        let transactions;
-        if(role !== 'admin') {
-            const userId = await pool.query(`SELECT user_id from users
-                                             WHERE username = $1` , [username]);
-            const queryString = `SELECT transactions.transaction_id,
-                                        transactions.transaction_type,
-                                        (SELECT username from users
-                                         where user_id = transactions.from_user_id)
-                                            as from_user,
-                                        (SELECT username from users
-                                         where user_id = transactions.to_user_id)
-                                            as to_user, transactions.amount
-                                 from transactions
-                                 WHERE from_user_id = $1 or to_user_id = $1`
-            transactions = await pool.query(queryString , [userId.rows[0].user_id]);
+        let whereClause = '';
+        let userId;
+        if(role === 'customer') {
+            userId = await pool.query(`SELECT user_id from users 
+                                                WHERE username = $1` , [username]);
+            whereClause = `WHERE from_user_id = ${userId.rows[0].user_id} or to_user_id = ${userId.rows[0].user_id}`;
         }
-        else {
-            const queryString = `SELECT transactions.transaction_id,
-                                        transactions.transaction_type,
-                                        (SELECT username from users
-                                         where user_id = transactions.from_user_id)
-                                            as from_user,
-                                        (SELECT username from users
-                                         where user_id = transactions.to_user_id)
-                                            as to_user, transactions.amount
-                                 from transactions`
-            transactions = await pool.query(queryString);
-        }
+        const queryString = `SELECT transactions.transaction_id,
+                                                      transactions.transaction_type,
+                                                      (SELECT username from users 
+                                                                       where user_id = transactions.from_user_id) 
+                                                          as from_user,
+                                                      (SELECT username from users 
+                                                                       where user_id = transactions.to_user_id)   
+                                                          as to_user, transactions.amount , transactions.status as transaction_status
+                                               from transactions` + ` ${whereClause}`;
+        const transactions = await pool.query(queryString);
         res.send({"data" : transactions.rows , "role": role});
     }catch (err){
         res.status(err.status || 400).send(err.message);
